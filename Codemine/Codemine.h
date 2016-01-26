5e6c7c17ea00590eab6fae5efc0f7ad68564d97d
Chris Combs <chco@nodes.dk>
2016-01-26 17:09:26 +0100

    Initial Commit

---
+++ Codemine/Codemine.h
@@ -0,0 +1,19 @@
+//
+//  Codemine.h
+//  Codemine
+//
+//  Created by Chris Combs on 26/01/16.
+//  Copyright © 2016 Nodes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Codemine.
+FOUNDATION_EXPORT double CodemineVersionNumber;
+
+//! Project version string for Codemine.
+FOUNDATION_EXPORT const unsigned char CodemineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Codemine/PublicHeader.h>
+
+
