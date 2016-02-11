f46267489b0eb6f41ceb67d12ba5bf32915e9ebb
Marius Constantinescu <constantinescu.marius@gmail.com>
2016-02-11 21:42:42 +0100

    improved email validation tests

--- CodemineTests/CodemineTests.swift
+++ CodemineTests/CodemineTests.swift
@@ -36,11 +36,17 @@ class CodemineTests: XCTestCase {
     // MARK: - StringExtensions tests
     
     func testTrueEmailAddress() {
-        XCTAssertTrue("john@nodes.dk".isValidEmailAddress())
+		let validEmails = ["email@example.com", "firstname.lastname@example.com", "email@subdomain.example.com", "firstname+lastname@example.com", "email@123.123.123.123", "email@[123.123.123.123]", "1234567890@example.com", "email@example-one.com", "_______@example.com", "email@example.name", "email@example.museum", "email@example.co.jp", "firstname-lastname@example.com"]
+		for emailAddress in validEmails {
+			XCTAssertTrue(emailAddress.isValidEmailAddress(), "email \(emailAddress) was considered valid, but it is not")
+		}
     }
     
     func testFalseEmailAddress() {
-        XCTAssertFalse("joh@n@nodes.dk".isValidEmailAddress())
+		let invalidEmails = ["plainaddress", "#@%^%#$@#$@#.com", "@example.com", "Joe Smith <email@example.com>", "email.example.com", "email@example@example.com", ".email@example.com", "email.@example.com", "email..email@example.com", "あいうえお@example.com", "email@example.com (Joe Smith)", "email@example", "email@-example.com", "email@example.web", "email@111.222.333.44444", "email@example..com", "Abc..123@example.com"]
+		for emailAddress in invalidEmails {
+			XCTAssertFalse(emailAddress.isValidEmailAddress(), "email \(emailAddress) was considered invalid, but it is not")
+		}
     }
     
 }
