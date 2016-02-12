247a18cd6e9d7661f81d09e57ea0efc3b3dc6af2
Marius Constantinescu <maco@nodes.dk>
2016-02-12 11:30:57 +0100

    Updated NSString+Ranges to Swift. Updated tests

--- Codemine/StringExtensions.swift
+++ Codemine/StringExtensions.swift
@@ -48,3 +48,53 @@ public extension String {
     }
 }
 
+
+public extension String {
+    private var range: Range<Index> {
+        return Range(start: self.startIndex, end: self.endIndex)
+    }
+    
+    private func containsRange(range: Range<Index>) -> Bool {
+        if range.startIndex < self.startIndex || range.endIndex > self.endIndex {
+            return false
+        }
+        
+        return true
+    }
+    
+    public enum RangeSearchType: Int {
+        case LeftToRight
+        case RightToLeft
+        case Broadest
+        case Narrowest
+    }
+    
+    /**
+     Returns the range between two substrings, including the 2 substrings.
+     
+     - parameter string:     first substring
+     - parameter toString:   second substring
+     - parameter searchType: direction of search
+     - parameter inRange:    the range of the string in which the search is performed. If nil, it will be done in the whole string.
+     
+     - returns: the range between the start of the first substring and the end of the last substring
+     */
+    public func rangeFromString(string: String, toString: String, searchType: RangeSearchType = .LeftToRight,  inRange: Range<Index>? = nil) -> Range<Index>? {
+        let range = inRange ?? self.range
+        if !containsRange(range) { return nil }
+        
+        guard let firstRange = self.rangeOfString(string, options: NSStringCompareOptions(rawValue: 0), range: range, locale: nil) else { return nil }
+        guard let secondRange = self.rangeOfString(toString, options: NSStringCompareOptions(rawValue: 0), range: range, locale: nil) else { return nil }
+        
+        switch searchType {
+        case .LeftToRight:
+            return Range(start: firstRange.startIndex, end: secondRange.endIndex)
+        default:
+            print("Other search options not yet implemented.")
+        }
+        
+        return nil
+    }
+    
+}
+
--- CodemineTests/CodemineTests.swift
+++ CodemineTests/CodemineTests.swift
@@ -36,17 +36,23 @@ class CodemineTests: XCTestCase {
     // MARK: - String Extensions tests
     
     func testTrueEmailAddress() {
-		let validEmails = ["email@example.com", "firstname.lastname@example.com", "email@subdomain.example.com", "firstname+lastname@example.com", "email@123.123.123.123", "email@[123.123.123.123]", "1234567890@example.com", "email@example-one.com", "_______@example.com", "email@example.name", "email@example.museum", "email@example.co.jp", "firstname-lastname@example.com"]
+		let validEmails = ["email@example.com", "firstname.lastname@example.com", "email@subdomain.example.com", "firstname+lastname@example.com", "1234567890@example.com", "email@example-one.com", "_______@example.com", "email@example.name", "email@example.museum", "email@example.co.jp", "firstname-lastname@example.com"]
 		for emailAddress in validEmails {
-			XCTAssertTrue(emailAddress.isValidEmailAddress(), "email \(emailAddress) was considered valid, but it is not")
+			XCTAssertTrue(emailAddress.isValidEmailAddress(), "the email address: \(emailAddress) was considered invalid, but it is not")
 		}
     }
     
     func testFalseEmailAddress() {
-		let invalidEmails = ["plainaddress", "#@%^%#$@#$@#.com", "@example.com", "Joe Smith <email@example.com>", "email.example.com", "email@example@example.com", ".email@example.com", "email.@example.com", "email..email@example.com", "あいうえお@example.com", "email@example.com (Joe Smith)", "email@example", "email@-example.com", "email@example.web", "email@111.222.333.44444", "email@example..com", "Abc..123@example.com"]
+		let invalidEmails = ["plainaddress", "#@%^%#$@#$@#.com", "@example.com", "Joe Smith <email@example.com>", "email.example.com", "email@example@example.com", ".email@example.com", "email.@example.com", "email..email@example.com", "あいうえお@example.com", "email@example.com (Joe Smith)", "email@example", "email@-example.com", "email@111.222.333.44444", "email@example..com", "Abc..123@example.com"]
 		for emailAddress in invalidEmails {
-			XCTAssertFalse(emailAddress.isValidEmailAddress(), "email \(emailAddress) was considered invalid, but it is not")
+			XCTAssertFalse(emailAddress.isValidEmailAddress(), "the email address: \(emailAddress) was considered valid, but it is not")
 		}
+    }
+    
+    func testRange() {
+        let str = "Hello world!"
+        let range = str.rangeFromString("e", toString: " w")
+        XCTAssertTrue(range?.startIndex == str.startIndex.advancedBy(1) && range?.endIndex == str.startIndex.advancedBy(7), "range = \(range)")
     }
 	
 	
@@ -68,27 +74,70 @@ class CodemineTests: XCTestCase {
 		XCTAssertEqual(rect.origin.y, 20)
 	}
 	
-	func testHeight() {
-		var rect = CGRect(x: 10, y: 10, width: 100, height: 100)
-		XCTAssertEqual(rect.height, rect.size.height)
-		
-		// FIXME: why is height and width private? Why can't I set rect.height?
-//		rect.height = 20
-//		XCTAssertEqual(rect.size.height, 20)
-	}
-	
-	func testWidth() {
-		var rect = CGRect(x: 10, y: 10, width: 100, height: 100)
-		XCTAssertEqual(rect.width, rect.size.width)
-		
-		// FIXME: why is height and width private? Why can't I set rect.width?
-//		rect.width = 20
-//		XCTAssertEqual(rect.size.width, 20)
-	}
-	
 	func testReversingSize() {
 		let rect = CGRect(x: 10, y: 10, width: 100, height: 200)
 		let reversedRect = rect.rectByReversingSize()
 		XCTAssertTrue(rect.size.height == reversedRect.size.width && rect.size.width == reversedRect.size.height)
 	}
+    
+    // MARK: - CGPoint extension tests
+    
+    func testIsClosePoint() {
+        let point1 = CGPoint(x: 0, y: 0)
+        let point2 = CGPoint(x: 0, y: 1)
+        let point3 = CGPoint(x: 1, y: 1)
+        let point4 = CGPoint(x: 10, y: 10)
+        XCTAssertTrue(point1.isCloseTo(point2, tolerance: 1))
+        XCTAssertFalse(point1.isCloseTo(point2, tolerance: 0.5))
+        XCTAssertTrue(point1.isCloseTo(point3, tolerance: 2))
+        XCTAssertFalse(point2.isCloseTo(point4, tolerance: 3))
+    }
+    
+    func testPointAddition() {
+        let point1 = CGPoint(x: 10, y: 11)
+        let point2 = CGPoint(x: 2, y: 13)
+        XCTAssertTrue((point1+point2).x == point1.x + point2.x && (point1+point2).y == point1.y + point2.y)
+        
+    }
+    
+    func testPointMultiplication() {
+        let point1 = CGPoint(x: 10, y: 11)
+        let point2 = CGPoint(x: 2, y: 13)
+        XCTAssertTrue((point1*point2).x == point1.x * point2.x && (point1*point2).y == point1.y * point2.y)
+        
+    }
+    
+    func testPointWithScalarMultiplication() {
+        let point1 = CGPoint(x: 10, y: 11)
+        let scalar : CGFloat = 4
+        XCTAssertTrue((point1*scalar).x == point1.x * scalar && (point1*scalar).y == point1.y * scalar)
+        
+    }
+    
+    func testPointSubtraction() {
+        let point1 = CGPoint(x: 10, y: 11)
+        let point2 = CGPoint(x: 2, y: 13)
+        XCTAssertTrue((point1-point2).x == point1.x - point2.x && (point1-point2).y == point1.y - point2.y)
+        
+    }
+    
+    func testPointDivision() {
+        let point1 = CGPoint(x: 10, y: 11)
+        let point2 = CGPoint(x: 2, y: 13)
+        XCTAssertTrue((point1/point2).x == point1.x / point2.x && (point1/point2).y == point1.y / point2.y)
+        
+        let point3 = CGPoint(x: 0, y: 0)
+        XCTAssertTrue((point1/point3).x == point1.x / point3.x && (point1/point3).y == point1.y / point3.y)
+        
+    }
+    
+    func testPointByScalarDivision() {
+        let point1 = CGPoint(x: 10, y: 11)
+        let scalar : CGFloat = 4
+        XCTAssertTrue((point1/scalar).x == point1.x / scalar && (point1/scalar).y == point1.y / scalar)
+        
+        let scalar2 : CGFloat = 0
+        XCTAssertTrue((point1/scalar2).x == point1.x / scalar2 && (point1/scalar2).y == point1.y / scalar2)
+        
+    }
 }
