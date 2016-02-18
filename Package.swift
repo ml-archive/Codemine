0d30e1ed063335aa1916be33cbf39f8f1dddbad8
Marius Constantinescu <constantinescu.marius@gmail.com>
2016-02-18 16:45:24 +0100

    Added simlinks in Sources

---
+++ Package.swift
@@ -0,0 +1,14 @@
+//
+//  Package.swift
+//  Codemine
+//
+//  Created by Marius Constantinescu on 18/02/16.
+//  Copyright © 2016 Nodes. All rights reserved.
+//
+
+import Foundation
+
+import PackageDescription
+let package = Package(
+    name: "Codemine"
+)
