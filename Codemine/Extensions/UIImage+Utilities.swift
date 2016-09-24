b6d94a2a2b342b44aeb5d17a7702afb11636a4ff
Chris Combs <chco@nodes.dk>
2016-09-24 21:56:33 +0200

    Removing force unwraps

--- Codemine/Extensions/UIImage+Utilities.swift
+++ Codemine/Extensions/UIImage+Utilities.swift
@@ -24,7 +24,7 @@ public extension UIImage {
      
      - Returns: A 'UIImage' with the specified color, size and corner radius.
      */
-    public class func imageFromColor(_ color: UIColor, size: CGSize, cornerRadius: CGFloat) -> UIImage {
+    public class func imageFromColor(_ color: UIColor, size: CGSize, cornerRadius: CGFloat) -> UIImage? {
         
         /// The base rectangle of the image.
         let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
@@ -42,12 +42,12 @@ public extension UIImage {
         UIGraphicsBeginImageContext(size)
         
         UIBezierPath(roundedRect: rect, cornerRadius:cornerRadius).addClip()
-        image? .draw(in: rect)
+        image?.draw(in: rect)
         
         image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         
-        return image!
+        return image
     }
     
     /**
@@ -61,7 +61,7 @@ public extension UIImage {
      - icon: The embedded image that will be on top.
      - Returns: The combined image as `UIImage`.
      */
-    public class func imageByEmbeddingIconIn(_ imageOne: UIImage, icon: UIImage) -> UIImage {
+    public class func imageByEmbeddingIconIn(_ imageOne: UIImage, icon: UIImage) -> UIImage? {
         let newSize = CGSize(width: imageOne.size.width, height: imageOne.size.height)
         UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
         
@@ -70,7 +70,7 @@ public extension UIImage {
         // Center icon
         icon.draw(in: CGRect(x: imageOne.size.width/2 - icon.size.width/2, y: imageOne.size.height/2 - icon.size.height/2, width: icon.size.width, height: icon.size.height), blendMode:CGBlendMode.normal, alpha:1.0)
         
-        let newImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
         return newImage
     }
     
@@ -80,13 +80,13 @@ public extension UIImage {
      
      - Returns: The orientation corrected image as an `UIImage`.
      */
-    public func rotationCorrectedImage() -> UIImage {
+    public func rotationCorrectedImage() -> UIImage? {
         //        if (self.imageOrientation == UIImageOrientation.Up) { return self }
         
-        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale);
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
         self.draw(in: CGRect(origin: CGPoint.zero, size: self.size))
-        let normalizedImage = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        return normalizedImage!;
+        let normalizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return normalizedImage
     }
 }
