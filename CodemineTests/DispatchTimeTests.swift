a033bbb649ac06145763ce6d2ecb76fe325afc9c
Marius Constantinescu <constantinescu.marius@gmail.com>
2017-01-13 15:29:06 +0100

    Added another test for the dispatch time extension

--- CodemineTests/DispatchTimeTests.swift
+++ CodemineTests/DispatchTimeTests.swift
@@ -25,4 +25,15 @@ class DispatchTimeTests: XCTestCase {
         
     }
     
+    func testAfterIntegerDispatchTime() {
+        let expectation = self.expectation(description: "The duration should be 3 seconds")
+        
+        DispatchQueue.main.asyncAfter(deadline: 3) {
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 3 + 0.005, handler: nil)
+        
+    }
+    
 }
